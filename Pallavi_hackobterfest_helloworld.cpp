5dfda43666f4a6374b0d50fdf1c4536957178af2
PALLAVI <108530842+Pallavipankaj@users.noreply.github.com>
2023-10-16 20:55:43 +0530

    Create Pallavi_hackobterfest_helloworld.cpp

---
+++ Pallavi_hackobterfest_helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! It's a month of October:festive season for open-source contributors." << std::endl;
+    return 0;
+}
+/*In this program:
+
+    #include <iostream>: This line includes the input/output stream library, which provides functions for input and output operations.
+    int main() { ... }: This is the main function where the execution of the program begins.
+    std::cout << "Hello, World!" << std::endl;: This line prints "Hello, World!" to the standard output (usually the console or terminal).
+    return 0;: This statement indicates that the program has executed successfully. The main function should return an integer value.8/
